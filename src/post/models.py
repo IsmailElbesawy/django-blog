0b31d25ef29bbf591456ebef295dce8b8b3815e2
IsmailElbesawy <esm197496@gmail.com>
2020-07-20 22:36:40 +0300

    after add post app to settings

--- src/post/models.py
+++ src/post/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 class Author (models.Model):
     user = models.OneToOneField(User, on_delete= models.CASCADE)
     profile_picture = models.ImageField()
+    
 
     def __str__(self):
         return self.user.username 

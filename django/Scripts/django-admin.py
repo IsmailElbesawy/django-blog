d6f01d0762a30685663788a345131a88d3812155
IsmailElbesawy <esm197496@gmail.com>
2020-07-20 22:21:27 +0300

    first commit

---
+++ django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ismail\desktop\tutorial\django-blog\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

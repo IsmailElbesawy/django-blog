1d084d2f14ebe3fe68d0a9278e3174cc152d3267
IsmailElbesawy <esm197496@gmail.com>
2020-07-23 01:08:03 +0300

    end pagination, sidebar, search, category_cont, in blog.html page

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,14 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from post.views import index, blog,post
+from post.views import index, blog, post, search
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index),
-    path('blog/',blog),
-    path('post/', post)
+    path('', index),
+    path('blog/', blog, name='post_list'),
+    path('post/<id>/', post, name='post_detail'),
+    path('search/', search, name='search'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
